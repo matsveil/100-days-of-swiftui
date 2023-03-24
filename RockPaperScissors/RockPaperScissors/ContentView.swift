8f72d0d80ce61449ab0b6a7d8db95543031d88e5
Matsvei Liapich <hello@matsveil.com>
2023-03-24 15:36:35 -0400

    Begin challenge for day 25

--- RockPaperScissors/RockPaperScissors/ContentView.swift
+++ RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -9,27 +9,125 @@ import SwiftUI
 
 struct ContentView: View {
     var moves = ["Rock", "Paper", "Scissors"]
+    @State private var appsMove = Int.random(in: 0...2)
     @State private var app = ""
-    @State private var win = false
+    @State private var shouldWin = Bool.random()
+    @State private var score = 0
+    @State private var question = 0
         
     var body: some View {
-        VStack {
-            Button("Rock", action: rockVsApp)
-            Button("Paper", action: paperVsApp)
-            Button("Scissors", action: scissorsVsApp)
+        NavigationView {
+            VStack {
+                Text("Rock Paper Scissors")
+                    .font(.largeTitle.bold())
+                HStack(spacing: 10) {
+                                        
+                    VStack {
+                        Text("YOU SHOULD")
+                            .font(.footnote)
+                        Text(shouldWin ? "Win" : "Lose")
+                            .font(.headline)
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(10)
+                    .background(.ultraThinMaterial)
+                    .clipShape(RoundedRectangle(cornerRadius: 25, style: .continuous))
+                    
+                    VStack(alignment: .center) {
+                        Text("APP'S MOVE")
+                            .font(.subheadline)
+                        Text(moves[appsMove])
+                            .font(.headline)
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(10)
+                    .background(.ultraThinMaterial)
+                    .clipShape(RoundedRectangle(cornerRadius: 25, style: .continuous))
+                    
+                    VStack {
+                        Text("SCORE")
+                            .font(.subheadline)
+                        Text(String(score))
+                            .font(.headline)
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(10)
+                    .background(.ultraThinMaterial)
+                    .clipShape(RoundedRectangle(cornerRadius: 25, style: .continuous))
+                }
+                
+                VStack(spacing: 20) {
+                    Button("🪨", action: rockVsApp)
+                        .font(.system(size: 100))
+                    Button("📄", action: paperVsApp)
+                        .font(.system(size: 100))
+                    Button("✂️", action: scissorsVsApp)
+                        .font(.system(size: 100))
+                }
+
+            }
         }
     }
     
     func rockVsApp() {
-        app = moves[Int.random(in: 0...2)]
+        app = moves[appsMove]
+        
+        if shouldWin {
+            if app == "Scissors" {
+                score += 1
+            } else {
+                score -= 1
+            }
+        } else {
+            if app == "Paper" {
+                score += 1
+            } else {
+                score -= 1
+            }
+        }
+        
+        shouldWin.toggle()
     }
-    
+        
     func paperVsApp() {
-        app = moves[Int.random(in: 0...2)]
+        app = moves[appsMove]
+        
+        if shouldWin {
+            if app == "Scissors" {
+                score += 1
+            } else {
+                score -= 1
+            }
+        } else {
+            if app == "Paper" {
+                score += 1
+            } else {
+                score -= 1
+            }
+        }
+        
+        shouldWin.toggle()
     }
-    
+        
     func scissorsVsApp() {
-        app = moves[Int.random(in: 0...2)]
+        app = moves[appsMove]
+
+        
+        if shouldWin {
+            if app == "Scissors" {
+                score += 1
+            } else {
+                score -= 1
+            }
+        } else {
+            if app == "Paper" {
+                score += 1
+            } else {
+                score -= 1
+            }
+        }
+        
+        shouldWin.toggle()
     }
 }
 
