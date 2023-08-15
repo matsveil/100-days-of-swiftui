29c41601679057acbcc28b56664d0bdf6d85dcea
Matsvei Liapich <hello@matsvei.com>
2023-08-15 01:36:04 -0400

    Complete day 36

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Matsvei Liapich on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
